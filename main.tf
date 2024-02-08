32b1317d4742116ad0bb51fe4cb017be62b1e7dc
bhokumar <bhoopendra.akgec@gmail.com>
2024-02-08 20:19:19 +0530

    Adding changes related to for_each and count

--- main.tf
+++ main.tf
@@ -40,136 +40,20 @@ resource "azurerm_resource_group" "app_rg_1101_dev01" {
   location = local.location
 }
 
-resource "azurerm_network_security_group" "network_security_group_dev1" {
-  name                = "network_security_group_dev01"
-  location            = azurerm_resource_group.app_rg_1101_dev01.location
-  resource_group_name = azurerm_resource_group.app_rg_1101_dev01.name
-
- depends_on = [ azurerm_resource_group.app_rg_1101_dev01 ]
-
- security_rule {
-     name = "SSH"
-     priority = 1000
-     direction = "Inbound"
-     access = "Allow"
-     protocol = "Tcp"
-     source_port_range = "*"
-     destination_port_range = "22"
-     source_address_prefix = "*"
-     destination_address_prefix = "*"
-   }
-
+resource "azurerm_storage_account" "storage_account_dev01" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.app_rg_1101_dev01.name
+  location                 = azurerm_resource_group.app_rg_1101_dev01.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"  
 }
 
-resource "azurerm_subnet_network_security_group_association" "subneta_network_security_group_association_dev1" {
-  subnet_id                 = azurerm_subnet.subneta.id
-  network_security_group_id = azurerm_network_security_group.network_security_group_dev1.id
-}
+resource "azurerm_storage_container" "dev1_containers" {
+  
+  for_each = toset([ "data", "files", "images" ])
 
-resource "azurerm_virtual_network" "virtual_network_dev1" {
-  name                = local.virtual_network.name
-  location            = azurerm_resource_group.app_rg_1101_dev01.location
-  resource_group_name = azurerm_resource_group.app_rg_1101_dev01.name
-  address_space       = [local.virtual_network.address_space]
-
-
-  tags = {
-    environment = "Production"
-  }
-  depends_on = [ azurerm_resource_group.app_rg_1101_dev01 ]
-}
-
-resource "azurerm_subnet" "subneta" {
-  name                 = local.virtual_network.subnets[0].name
-  resource_group_name  = azurerm_resource_group.app_rg_1101_dev01.name
-  virtual_network_name = azurerm_virtual_network.virtual_network_dev1.name
-  address_prefixes     = [local.virtual_network.subnets[0].address_prefix]
-  depends_on = [ azurerm_virtual_network.virtual_network_dev1 ]
-}
-
-
-resource "azurerm_subnet" "subnetb" {
-  name                 = local.virtual_network.subnets[1].name
-  resource_group_name  = azurerm_resource_group.app_rg_1101_dev01.name
-  virtual_network_name = azurerm_virtual_network.virtual_network_dev1.name
-  address_prefixes     = [local.virtual_network.subnets[1].address_prefix]
-  depends_on = [ azurerm_virtual_network.virtual_network_dev1 ]
-}
-
-resource "azurerm_public_ip" "app_ip_dev1" {
-  name                    = local.dev1_public_ip.name
-  location                = azurerm_resource_group.app_rg_1101_dev01.location
-  resource_group_name     = azurerm_resource_group.app_rg_1101_dev01.name
-  allocation_method       = local.dev1_public_ip.allocation_method
-  idle_timeout_in_minutes = 30
-
-  tags = {
-    environment = "test"
-  }
-}
-
-resource "azurerm_network_interface" "app_interface_dev1" {
-  name                = local.dev1_network_interface.name
-  location            = azurerm_resource_group.app_rg_1101_dev01.location
-  resource_group_name = azurerm_resource_group.app_rg_1101_dev01.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subneta.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.app_ip_dev1.id
-  }
-
-  depends_on = [ azurerm_subnet.subneta ]
-}
-
-resource "tls_private_key" "linuxkey" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "linuxpemkey" {
-  filename = "linuxkey.pem"
-  content  = tls_private_key.linuxkey.private_key_pem
-  depends_on = [ tls_private_key.linuxkey ]
-}
-resource "azurerm_linux_virtual_machine" "dev1_windows_server" {
-  name                = "dev1appvm"
-  resource_group_name = azurerm_resource_group.app_rg_1101_dev01.name
-  location            = azurerm_resource_group.app_rg_1101_dev01.location
-  size                = "Standard_D2S_v3"
-  admin_username      = "linuxuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.app_interface_dev1.id,
-  ]
-
-  admin_ssh_key {
-    username   = "linuxuser"
-    public_key = tls_private_key.linuxkey.public_key_openssh
-  }
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-
-  depends_on = [ 
-    azurerm_network_interface.app_interface_dev1, 
-    azurerm_resource_group.app_rg_1101_dev01,
-    tls_private_key.linuxkey
-    ]
-}
-
-
-
-
-output "subnet_id" {
-  value = azurerm_subnet.subneta.id
+  name                  = each.key
+  storage_account_name  = azurerm_storage_account.storage_account_dev01.name
+  container_access_type = "private"
+  
 }
