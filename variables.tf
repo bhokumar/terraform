fff0b7be93e83348e06ea0d5a9b7b3151f045c83
bhokumar <bhoopendra.akgec@gmail.com>
2024-02-10 20:19:19 +0530

    Refactoring files

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "client_secret" {
+  type = string
+  sensitive = true
+  default = "*******************************"
+}
+
+variable "client_id" {
+  type = string
+  sensitive = true
+  default = "*******************************"
+}
+
+variable "tenant_id" {
+    type = string
+    sensitive = true
+    default = "*******************************"
+}
+
+variable "subscription_id" {
+  type = string
+  sensitive = true
+  default = "*******************************"
+}
