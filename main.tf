6ab48308a071d82b7044c32d13d52d8d03d8f1f0
bhokumar <bhoopendra.akgec@gmail.com>
2024-02-11 20:19:19 +0530

    Adding rules for nsg

--- main.tf
+++ main.tf
@@ -49,8 +49,37 @@ resource "azurerm_network_security_group" "network_security_group_dev1" {
   location            = azurerm_resource_group.app_rg_1101_dev01.location
   resource_group_name = azurerm_resource_group.app_rg_1101_dev01.name
 
-depends_on = [ azurerm_resource_group.app_rg_1101_dev01 ]
+ depends_on = [ azurerm_resource_group.app_rg_1101_dev01 ]
 
+ security_rule {
+     name = "RDP"
+     priority = 1000
+     direction = "Inbound"
+     access = "Allow"
+     protocol = "Tcp"
+     source_port_range = "*"
+     destination_port_range = "3389"
+     source_address_prefix = "*"
+     destination_address_prefix = "*"
+   }
+
+  security_rule {
+        name = "HTTP"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "80"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "subneta_network_security_group_association_dev1" {
+  subnet_id                 = azurerm_subnet.subneta.id
+  network_security_group_id = azurerm_network_security_group.network_security_group_dev1.id
 }
 
 resource "azurerm_virtual_network" "virtual_network_dev1" {
