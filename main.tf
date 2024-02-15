1875742fd1d55059ffde55031877b9af0ff280e6
bhokumar <bhoopendra.akgec@gmail.com>
2024-02-15 20:19:19 +0530

    Adding changes for virtual network

--- main.tf
+++ main.tf
@@ -8,40 +8,61 @@ terraform {
 }
 
 
-
-
-resource "azurerm_resource_group" "app_rg_1101_dev01" {
-  name     = "RG_1101_DEV01"
+locals {
+  resource_group_name = "RG_1101_DEV01"
   location = "Central India"
+  storage_account_name = "storageaccount1101dev01"
+  container_name = "gsspcontainer"
+  blob_name = "main.tf"
+
+  virtual_network = {
+    name = "virtual_network_dev01"
+    address_space = "10.0.0.0/16"
+    subnets = [
+      {
+        name = "subneta"
+        address_prefix = "10.0.0.0/24"
+      },
+      {
+        name = "subnetb"
+        address_prefix = "10.0.1.0/24"
+      } 
+    ]
+  }
+
+}
+resource "azurerm_resource_group" "app_rg_1101_dev01" {
+  name     = local.resource_group_name
+  location = local.location
 }
 
+resource "azurerm_network_security_group" "network_security_group_dev1" {
+  name                = "network_security_group_dev01"
+  location            = azurerm_resource_group.app_rg_1101_dev01.location
+  resource_group_name = azurerm_resource_group.app_rg_1101_dev01.name
 
+depends_on = [ azurerm_resource_group.app_rg_1101_dev01 ]
 
-resource "azurerm_storage_account" "storage_dev01" {
-  name                     = "storageaccount1101dev01"
-  resource_group_name      = azurerm_resource_group.app_rg_1101_dev01.name
-  location                 = azurerm_resource_group.app_rg_1101_dev01.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind = "StorageV2"
+}
+
+resource "azurerm_virtual_network" "virtual_network_dev1" {
+  name                = local.virtual_network.name
+  location            = azurerm_resource_group.app_rg_1101_dev01.location
+  resource_group_name = azurerm_resource_group.app_rg_1101_dev01.name
+  address_space       = [local.virtual_network.address_space]
+
+  subnet {
+    name           = local.virtual_network.subnets[0].name
+    address_prefix = local.virtual_network.subnets[0].address_prefix
+  }
+
+  subnet {
+    name           = local.virtual_network.subnets[1].name
+    address_prefix = local.virtual_network.subnets[1].address_prefix
+  }
 
   tags = {
-    environment = "staging"
+    environment = "Production"
   }
   depends_on = [ azurerm_resource_group.app_rg_1101_dev01 ]
-}
-
-resource "azurerm_storage_container" "gsspcontainer" {
-  name                  = "gsspcontainer"
-  storage_account_name  = azurerm_storage_account.storage_dev01.name
-  container_access_type = "blob"
-  depends_on = [ azurerm_storage_account.storage_dev01 ]
-}
-
-resource "azurerm_storage_blob" "maintf" {
-  name                   = "main.tf"
-  storage_account_name   = azurerm_storage_account.storage_dev01.name
-  storage_container_name = azurerm_storage_container.gsspcontainer.name
-  type                   = "Block"
-  source                 = "main.tf"
 }
---
+++ main_storage_account.tf_v1
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.92.0"
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "app_rg_1101_dev01" {
+  name     = "RG_1101_DEV01"
+  location = "Central India"
+}
+
+
+
+resource "azurerm_storage_account" "storage_dev01" {
+  name                     = "storageaccount1101dev01"
+  resource_group_name      = azurerm_resource_group.app_rg_1101_dev01.name
+  location                 = azurerm_resource_group.app_rg_1101_dev01.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+
+  tags = {
+    environment = "staging"
+  }
+  depends_on = [ azurerm_resource_group.app_rg_1101_dev01 ]
+}
+
+resource "azurerm_storage_container" "gsspcontainer" {
+  name                  = "gsspcontainer"
+  storage_account_name  = azurerm_storage_account.storage_dev01.name
+  container_access_type = "blob"
+  depends_on = [ azurerm_storage_account.storage_dev01 ]
+}
+
+resource "azurerm_storage_blob" "maintf" {
+  name                   = "main.tf"
+  storage_account_name   = azurerm_storage_account.storage_dev01.name
+  storage_container_name = azurerm_storage_container.gsspcontainer.name
+  type                   = "Block"
+  source                 = "main.tf"
+}
